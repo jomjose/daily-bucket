b9a88b861a4ddc0105d3cc1d178ea8526662f213
Jom Jose <jomjose@users.noreply.github.com>
2014-04-23 23:25:35 +0530

    Create main.js

---
+++ resize-image-canvas/main.js
@@ -0,0 +1,26 @@
+var MAX_HEIGHT = 100;
+function render(src){
+	var image = new Image();
+	image.onload = function(){
+		var canvas = document.getElementById("myCanvas");
+		if(image.height > MAX_HEIGHT) {
+			image.width *= MAX_HEIGHT / image.height;
+			image.height = MAX_HEIGHT;
+		}
+		var ctx = canvas.getContext("2d");
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		canvas.width = image.width;
+		canvas.height = image.height;
+		ctx.drawImage(image, 0, 0, image.width, image.height);
+	};
+	image.src = src;
+}
+
+/*/*
+/* Create a JavaScript Image object.
+/* Attach a handler to the onload event of that Image.
+/* Check to see what the dimensions of the loaded image is, and if the original image's height is greater than our maximum allowed, change those dimensions.
+/* Clear anything that is in our canvas element.
+/* Set the canvas dimensions to the dimensions of the Image, and
+/* Draw the image to the canvas.
+/*/*
